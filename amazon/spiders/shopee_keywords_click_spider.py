9d812c8ee08a04ea7a05b9fbae42f5c58df92430
wosunmo <wosunmo@gmail.com>
2021-01-23 23:09:50 +0800

    submit

--- amazon/spiders/shopee_keywords_click_spider.py
+++ amazon/spiders/shopee_keywords_click_spider.py
@@ -1,3 +1,10 @@
+import urllib
+from urllib import parse
+import scrapy
+from scrapy.http import Request
+import urllib
+from urllib import parse
+
 import scrapy
 from scrapy.http import Request
 
@@ -6,25 +13,23 @@ class KeyWordsSpider(scrapy.Spider):
     name = "shopee_keywords_click"
     seed = "sofa"
     allowed_domains = ["shopee.com.my"]
-    start_urls = [
-        "https://shopee.com.my/Women's-bag-single-shoulder-diagonal-bag-Single-Shoulder-Handbag-Shoulder-Bag女包斜挎包-单肩手提包-i.343911237.3271685945"]
+    start_urls = ["https://shopee.com.my/search?keyword=casual%20bag"]
 
     """
     解析当前页bestsellers的所有数据
     """
 
     def parse(self, response):
-        # list = response.xpath(".//div[@class='col-xs-2-4 shopee-search-item-result__item']")
-        # for item in list:
-        #     url = item.xpath(".//a[@data-sqe='link']/@href").extract_first()
-        #     url = parse.urljoin(response.url, url)
-        #     url = urllib.parse.unquote(url, encoding='utf-8', errors='replace')
-        #     print("url: " + url)
-        #     yield Request(url=url, callback=self.parse, dont_filter=True)
-
-        i = 0
-        while i <= 100:
-            i += 1;
-            yield Request(url=response.url, callback=self.parse, dont_filter=True)
-
+        list = response.xpath(".//div[@class='col-xs-2-4 shopee-search-item-result__item']")
+        for item in list:
+            ad = item.xpath(".//div[@class='_2uCaTM']")
+            if ad:
+                url = item.xpath(".//a[@data-sqe='link']/@href").extract_first()
+                url = parse.urljoin(response.url, url)
+                url = urllib.parse.unquote(url, encoding='utf-8', errors='replace')
+                print("url: " + url)
+                i = 0
+                while i <= 100:
+                    i += 1
+                    yield Request(url=response.url, callback=self.parse, dont_filter=True)
     pass
